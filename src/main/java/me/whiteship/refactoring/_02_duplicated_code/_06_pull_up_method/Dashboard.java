ef5827dd0eabec72dbd9ce73b3f9b42afd9f3200
k-gn <rlarbska97@gmail.com>
2022-03-08 14:37:37 +0900

    중복 제거 - pull up method

---
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
@@ -0,0 +1,36 @@
+package me.whiteship.refactoring._02_duplicated_code._06_pull_up_method;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+// 여러 하위 클래스에 동일한 코드가 있다면 적용 (상위 클래스로 올린다)
+public class Dashboard {
+
+    public static void main(String[] args) throws IOException {
+        ReviewerDashboard reviewerDashboard = new ReviewerDashboard();
+        reviewerDashboard.printReviewers();
+
+        ParticipantDashboard participantDashboard = new ParticipantDashboard();
+        participantDashboard.printUsernames(15);
+    }
+
+    // refactoring - pull members up
+    public void printUsernames(int eventId) throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+
+        // Get participants
+        Set<String> participants = new HashSet<>();
+        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+
+        // Print participants
+        participants.forEach(System.out::println);
+    }
+}
---
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/ParticipantDashboard.java
@@ -0,0 +1,13 @@
+package me.whiteship.refactoring._02_duplicated_code._06_pull_up_method;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ParticipantDashboard extends Dashboard {
+
+}
