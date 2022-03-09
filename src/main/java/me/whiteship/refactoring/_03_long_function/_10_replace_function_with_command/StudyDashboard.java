36ec01d025c75e45f270d961130e484c6987d50b
k-gn <rlarbska97@gmail.com>
2022-03-09 11:06:34 +0900

    긴 함수 리팩토링 - 함수를 명령어로 바꾸기 (커맨드 패턴)

---
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_10_replace_function_with_command/StudyDashboard.java
@@ -0,0 +1,85 @@
+package me.whiteship.refactoring._03_long_function._10_replace_function_with_command;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// 함수를 명령으로 바꾸기
+// 함수를 독자적인 객체인 Command로 만들어 사용
+// 커맨드 패턴을 적용할 때의 이점
+// 1. 부가직인 기능으로 undo 기능을 만들수도 있다.
+// 2. 더 복잡한 기능을 구현하는데 필요한 여러 메소드를 추가할 수 있다.
+// 3. 상속이나 템플릿을 활용할 수 있다.
+// 4. 복잡한 메소드를 쪼갤 수 있다.
+public class StudyDashboard {
+
+    private final int totalNumberOfEvents;
+
+    public StudyDashboard(int totalNumberOfEvents) {
+        this.totalNumberOfEvents = totalNumberOfEvents;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        StudyDashboard studyDashboard = new StudyDashboard(15);
+        studyDashboard.print();
+    }
+
+    private Participant findParticipant(String username, List<Participant> participants) {
+        Participant participant = null;
+        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
+            participant = new Participant(username);
+            participants.add(participant);
+        } else {
+            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
+        }
+        return participant;
+    }
+
+    private void print() throws IOException, InterruptedException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        List<Participant> participants = new CopyOnWriteArrayList<>();
+
+        ExecutorService service = Executors.newFixedThreadPool(8);
+        CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
+
+        for (int index = 1 ; index <= totalNumberOfEvents ; index++) {
+            int eventId = index;
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        GHIssue issue = repository.getIssue(eventId);
+                        List<GHIssueComment> comments = issue.getComments();
+
+                        for (GHIssueComment comment : comments) {
+                            Participant participant = findParticipant(comment.getUserName(), participants);
+                            participant.setHomeworkDone(eventId);
+                        }
+
+                        latch.countDown();
+                    } catch (IOException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                }
+            });
+        }
+
+        latch.await();
+        service.shutdown();
+
+        new StudyPrinter(this.totalNumberOfEvents, participants).execute();
+    }
+
+}
