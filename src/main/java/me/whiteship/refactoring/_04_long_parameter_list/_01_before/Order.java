390f1c6bfd395748c466b998e59085f615a8fe87
k-gn <rlarbska97@gmail.com>
2022-03-10 11:59:00 +0900

    before test

--- src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_01_before/Order.java
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_01_before/Order.java
@@ -1,6 +1,8 @@
 package me.whiteship.refactoring._04_long_parameter_list._01_before;
 
 // 긴 매개변수 목록 리펙토링 before
+// 매개변수가 많을수록 함수의 역할을 이해하기 어렵다
+// 함수가 한가지 일만 하고있는지, 불필요한 매개변수인지, 뭉칠 수 있는 매개변수 목록이 있는지 고려
 public class Order {
 
     private int quantity;
