6cc8f94fd629a0a12d03c1b71380865c99aaf886
k-gn <rlarbska97@gmail.com>
2022-03-12 11:58:27 +0900

    값 객체를 변경 시 새 인스턴스 리턴

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/Person.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/Person.java
@@ -17,8 +17,15 @@ public class Person {
         return this.officeTelephoneNumber.areaCode();
     }
 
+    public void officeAreaCode(String areaCode) {
+        this.officeTelephoneNumber = new TelephoneNumber(areaCode, this.officeNumber());
+    }
+
     public String officeNumber() {
         return this.officeTelephoneNumber.number();
     }
 
+    public void officeNumber(String number) {
+        this.officeTelephoneNumber = new TelephoneNumber(this.officeAreaCode(), number);
+    }
 }
