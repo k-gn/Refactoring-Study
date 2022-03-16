345cfd5be9fce9a8d6c424f7c9bba2a2d1a024e1
k-gn <rlarbska97@gmail.com>
2022-03-16 21:51:14 +0900

    no message

--- src/main/java/me/whiteship/refactoring/_12_repeated_swtiches/SwitchImprovements.java
+++ src/main/java/me/whiteship/refactoring/_12_repeated_swtiches/SwitchImprovements.java
@@ -6,13 +6,13 @@ package me.whiteship.refactoring._12_repeated_swtiches;
 public class SwitchImprovements {
 
     public int vacationHours(String type) {
-        int result;
-        switch (type) {
-            case "full-time": result = 120;
-            case "part-time": result = 80;
-            case "temporal": result = 32;
-            default: result = 0;
-        }
+        // java 17 버젼에서 가능한 형태
+        int result = switch (type) {
+            case "full-time" -> 120;
+            case "part-time" -> 80;
+            case "temporal" -> 32;
+            default -> 0;
+        };
         return result;
     }
 }
