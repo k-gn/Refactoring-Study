2390b90cc4d8c86e81b0d112f7e4fa78f0c7b4be
k-gn <rlarbska97@gmail.com>
2022-03-17 16:59:11 +0900

    추측성 일반화 리팩토링 - 죽은코드 제거하기

--- src/main/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/Reservation.java
+++ src/main/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/Reservation.java
@@ -2,6 +2,15 @@ package me.whiteship.refactoring._15_speculative_generality._35_remove_dead_code
 
 import java.time.LocalDateTime;
 
+// 추측성 일반화
+// 나중에 이런저런 기능이 생길 것으로 예상해서 여러 경우에 필요로 할만한 기능을 만들어 놨지만 결국 쓰이지 않는 코드가 발생할 경우
+// You are not gonna need it
+
+// 죽은 코드 제거하기
+// 사용하지 않는 코드가 성능이나 기능에 영향을 끼치지는 않지만, 동작을 이해하는데 어려울 수 있다
+// 테스트 코드에서만 참조하고 있을 경우도 해당
+// inlay 설정으로 어디서 쓰이는지 확인 가능
+// 지워도 나중에 git 과 같은 버전관리 시스템으로 복원할 수 있다.
 public class Reservation {
 
     private String title;
