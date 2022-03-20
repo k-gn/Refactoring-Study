0f71ffaa563ce554b90826584ae8c5babbebe105
k-gn <rlarbska97@gmail.com>
2022-03-20 14:25:16 +0900

    no message

---
+++ src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
@@ -0,0 +1,19 @@
+package me.whiteship.refactoring._24_comments._43_introduce_assertion;
+
+// 주석 리팩토링
+public class Customer {
+
+    private Double discountRate;
+
+    public double applyDiscount(double amount) {
+        return (this.discountRate != null) ? amount - (this.discountRate * amount) : amount;
+    }
+
+    public Double getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(Double discountRate) {
+        this.discountRate = discountRate;
+    }
+}
